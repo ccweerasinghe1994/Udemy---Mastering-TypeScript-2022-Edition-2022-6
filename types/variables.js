c99aa2e2ad839c45d81a6401fe660e4a3783f0bc
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-31 11:07:58 +0530

    adding new varaibles

---
+++ types/variables.js
@@ -0,0 +1,12 @@
+var movie = 'abc';
+movie.length;
+// movie = 12;
+movie.toUpperCase();
+var age = 12;
+// age = "twio"
+var isValid;
+isValid = false;
+console.log(isValid.valueOf());
+
+const movies = ['1', '2', '3'];
+
--- types/variables.ts
+++ types/variables.ts
@@ -1,9 +1,24 @@
-let movie:string = 'abc';
-movie.length
+let movie: string = 'abc';
+movie.length;
 // movie = 12;
 
-movie.toUpperCase()
+movie.toUpperCase();
 
-let age:number = 12
+let age: number = 12;
 
 // age = "twio"
+
+let isValid: boolean;
+
+isValid = false;
+
+console.log(isValid.valueOf());
+
+
+let movieName: string;
+
+for (const movie of movies) {
+  if (movie === '1') {
+    movieName = 'abc';
+  }
+}
